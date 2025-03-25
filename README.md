19dc7171fc97b255c9731dd22729c44b085c1d5c
Boukada Adel <65848859+Leknif571@users.noreply.github.com>
2025-03-25 16:00:38 +0100

    Ajust refresh after add User

---
+++ README.md
@@ -0,0 +1,25 @@
+### Pour lancer le projet
+
+Lancer la commande suivante dans le terminal :
+
+```
+docker-compose up --build
+```
+
+Acceder à l'application via l'url suivante :
+
+```
+http://localhost:5173
+```
+
+Pour faire des tests sur le backend, vous pouvez utiliser l'url suivante pour accéder au playground de graphql :
+
+```
+http://localhost:3000/graphql
+```
+
+Pour l'accées au server NGinx pour retrouver les fichiers statics, vous pouvez utiliser l'url suivante :
+
+```
+http://localhost/
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import client from "./apolloClient"; // Assurez-vous que ce fichier est bien configuré
 import "./App.css";
 
 function App() {
-  const GET_HELLO = gql`
+  const GET_INFO = gql`
     query {
       findAll {
         _id
@@ -14,15 +15,47 @@ function App() {
     }
   `;
 
-  const { loading, error, data } = useQuery(GET_HELLO, { client, fetchPolicy: "network-only", });
+  const ADD_USER = gql`
+  mutation CreateApp($name: String!, $email: String!){
+    createApp(name: $name, email: $email) {
+      _id
+      name
+      email
+    }
+  }
+`;
+
+  const { loading, error, data } = useQuery(GET_INFO, { client, fetchPolicy: "network-only"});
+  const [createUser, { datau, loadingu, erroru }] = useMutation(ADD_USER, {client, fetchPolicy: "network-only", refetchQueries: [{ query: GET_INFO }],
+  });
+
+  const handleClick = () => {
+    let urandom = uuidv4();
+
+    createUser({
+      variables: { name: urandom, email: urandom+"@example.com"},
+    })
+      .then((response) => {
+        console.log("Utilisateur ajouté:", response.data.createApp);
+      })
+      .catch((err) => {
+        console.error("Erreur:", err);
+      });
+  };
 
   const  [display, setDisplay] = useState(false);
+
+
 
   return (
     <>
       <h1>Données de la DB</h1>
       {loading && <p>Chargement...</p>}
       {error && <p>Erreur: {error.message}</p>}
+
+      <button disabled={loadingu} onClick={handleClick}>
+        {loadingu ? 'Ajout en cours...' : 'Clique ici pour ajouter un utilisateur'}
+      </button>
 
       <ul>
         {data?.findAll.map(({ _id, name, email }) => (
@@ -33,11 +66,11 @@ function App() {
       </ul>
 
       <h1>Asset</h1>
-      <button onClick={() => setDisplay(!display)}>Clique ici pour afficher une image du fichier Asset</button>
+      <button style={{ backgroundColor: "blue", color: "white", padding: "10px", border: "none", borderRadius: "5px" }} onClick={() => setDisplay(!display)}>Clique ici pour afficher une image du fichier Asset</button>
       {display && (
         <div>
           <video width="640" height="360" controls autoPlay>
-            <source src="http://localhost:8080/video/video.mp4" type="video/mp4" />
+            <source src="http://localhost/video/video.mp4" type="video/mp4" />
             Votre navigateur ne supporte pas la lecture des vidéos.
           </video>
         </div>
